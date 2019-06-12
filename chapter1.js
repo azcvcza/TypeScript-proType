d8ceeced9d0526f54c6707ebcac307e2e6c082e9
韦粤信 <283940802@qq.com>
2019-06-12 11:35:39 +0800

    try array[] type

--- chapter1.js
+++ chapter1.js
@@ -6,3 +6,20 @@ var sayHello = function (name) {
     console.log("hello + ", name);
 };
 sayHello(sherlock.name);
+var VehicleType;
+(function (VehicleType) {
+    VehicleType[VehicleType["pedalCycle"] = 0] = "pedalCycle";
+    VehicleType[VehicleType["MotorCycle"] = 1] = "MotorCycle";
+    VehicleType[VehicleType["Car"] = 2] = "Car";
+    VehicleType[VehicleType["Van"] = 3] = "Van";
+    VehicleType[VehicleType["Bus"] = 4] = "Bus";
+    VehicleType[VehicleType["Lorry"] = 5] = "Lorry";
+})(VehicleType || (VehicleType = {}));
+var t_type = VehicleType.Lorry;
+var typeName = VehicleType[t_type];
+var monuments = [];
+monuments.push({
+    name: 'statue of liberty',
+    heightInMeters: 46
+});
+sayHello(monuments[0].name);
--- chapter1.ts
+++ chapter1.ts
@@ -17,3 +17,29 @@ const sayHello:Function = (name)=>{
 	console.log("hello + ",name);
 }
 sayHello(sherlock.name);
+
+enum VehicleType  {
+	pedalCycle,
+	MotorCycle,
+	Car,
+	Van,
+	Bus,
+	Lorry
+}
+
+const t_type = VehicleType.Lorry;
+
+const typeName = VehicleType[t_type];
+
+interface Monument{
+	name:string;
+	heightInMeters
+}
+
+const monuments:Monument[] = [];
+
+monuments.push({
+	name:'statue of liberty',
+	heightInMeters:46
+})
+sayHello(monuments[0].name)
